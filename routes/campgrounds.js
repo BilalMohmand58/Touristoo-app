e1dc2ff20a14bfeed9da30c3b26310a46a6bc472
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2022-06-29 12:05:21 +0500

    search

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,35 +1,81 @@
-const express = require('express') ; 
-const router = express.Router(); 
-const campgrounds = require('../controllers/campgrounds');
-const catchAsync = require ('../utilities/catchAsync');
-const ExpressError = require('../utilities/ExpressError');
-const Campground = require('../models/campground');
-const{isLoggedin,isAuthor,validateCampground} = require('../middleware');
-const multer = require('multer');
-const {storage} = require('../cloudinary')
-const upload = multer({storage});
+const express = require("express");
+const router = express.Router();
+const campgrounds = require("../controllers/campgrounds");
+const catchAsync = require("../utilities/catchAsync");
+const ExpressError = require("../utilities/ExpressError");
+const Campground = require("../models/campground");
+const { isLoggedin, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
+// Search Campground
 
+// Define escapeRegex function for search feature
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
+//INDEX - show all campgrounds
+router.get("/", function (req, res) {
+  if (req.query.search && req.xhr) {
+    const regex = new RegExp(escapeRegex(req.query.search), "gi");
+    // Get all campgrounds from DB
+    Campground.find({ name: regex }, function (err, allCampgrounds) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json(allCampgrounds);
+      }
+    });
+  } else {
+    // Get all campgrounds from DB
+    Campground.find({}, function (err, allCampgrounds) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (req.xhr) {
+          res.json(allCampgrounds);
+        } else {
+          res.render("campgrounds/index", {
+            campgrounds: allCampgrounds,
+            page: "campgrounds",
+          });
+        }
+      }
+    });
+  }
+});
 
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedin,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
-router.route('/')
-    .get(catchAsync(campgrounds.index))
-    .post(isLoggedin, upload.array('image'), validateCampground , catchAsync(campgrounds.createCampground))
-    // .post(upload.array('image'),(req , res)=>{
-    //     console.log(req.body, req.files); 
-    //     res.send('it worked!');
-    // })
+router.get("/new", isLoggedin, campgrounds.renderNewForm);
 
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedin,
+    isAuthor,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedin, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get('/new', isLoggedin,campgrounds.renderNewForm)
+router.get(
+  "/:id/edit",
+  isLoggedin,
+  isAuthor,
+  catchAsync(campgrounds.renderEditForm)
+);
 
-router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put (isLoggedin, isAuthor, upload.array('image'), validateCampground ,  catchAsync(campgrounds.updateCampground))
-    .delete (isLoggedin,isAuthor, catchAsync(campgrounds.deleteCampground))
-
-router.get('/:id/edit', isLoggedin, isAuthor, catchAsync( campgrounds.renderEditForm))
-
-
-module.exports = router ; 
+module.exports = router;
